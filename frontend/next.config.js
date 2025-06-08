fcb4356823fb86c2c5c1f20a6a49479af875d3cc
y-hirai28 <pyns28@gmail.com>
2025-06-08 15:38:03 +0900

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     
     env: {
         // Reference a variable that was defined in the .env file and make it available at Build Time
-        API_ENDPOINT: process.env.API_ENDPOINT,
+        NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
       },
 }
 
